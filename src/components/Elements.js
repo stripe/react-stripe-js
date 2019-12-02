52602c6540f3ae3cea106ce044968aa58a2cf91a
David Weedon <dweedon@stripe.com>
2019-12-02 14:55:56 -0600

    drop comments

--- src/components/Elements.js
+++ src/components/Elements.js
@@ -42,16 +42,12 @@ const validateStripe = (maybeStripe: $NonMaybeType<mixed>) => {
 };
 
 const usePrevious = (value) => {
-  // The ref object is a generic container whose current property is mutable ...
-  // ... and can hold any value, similar to an instance property on a class
   const ref = useRef();
 
-  // Store current value in ref
   useEffect(() => {
     ref.current = value;
-  }, [value]); // Only re-run if value changes
+  }, [value]);
 
-  // Return previous value (happens before update in useEffect above)
   return ref.current;
 };
 
