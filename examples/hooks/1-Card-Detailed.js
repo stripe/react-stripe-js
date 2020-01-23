474a0a359c17d9bf6fcc591fdb763d55ece3252b
David Weedon <dweedon@stripe.com>
2020-01-23 13:52:58 -0600

    pass through disabled

--- examples/hooks/1-Card-Detailed.js
+++ examples/hooks/1-Card-Detailed.js
@@ -62,11 +62,11 @@ const Field = ({
   </div>
 );
 
-const SubmitButton = ({processing, error, children}) => (
+const SubmitButton = ({processing, error, children, disabled}) => (
   <button
     className={`SubmitButton ${error ? 'SubmitButton--error' : ''}`}
     type="submit"
-    disabled={processing}
+    disabled={processing || disabled}
   >
     {processing ? 'Processing...' : children}
   </button>
