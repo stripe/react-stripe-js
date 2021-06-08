e5aae942659d10e144584a46f3ce62983ef913a0
Christopher Henn <christopher@stripe.com>
2021-06-08 07:45:35 -0700

    Upgrade Storybook to 6.x.x

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,3 @@
-const path = require('path');
-
 module.exports = {
   stories: ['./example.stories.js'],
-  addons: [
-    {
-      name: '@storybook/preset-typescript',
-      options: {include: [path.resolve(__dirname, '..')]},
-    },
-  ],
 };
