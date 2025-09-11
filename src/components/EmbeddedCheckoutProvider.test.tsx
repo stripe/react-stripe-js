46695fdbf7f2df80abb10cb51ea90396b2d08fca
Ben Anderson <ben@delivereasy.co.nz>
2025-09-10 23:11:57 -0400

    Convert React imports to use the recommended import syntax
    Doing so prevents any named imports leaking into the output type
    definitions (.d.ts files) and follows with what seems to be the React
    official recommendations [1].
    
    The more 'correct' fix would be only import those components that are
    needed by each component, but that's a bigger change.
    
    [1] https://github.com/facebook/react/pull/18102
    
    Fixes https://github.com/stripe/react-stripe-js/issues/569

--- src/components/EmbeddedCheckoutProvider.test.tsx
+++ src/components/EmbeddedCheckoutProvider.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import {render, act} from '@testing-library/react';
 import {renderHook} from '@testing-library/react-hooks';
 
--- src/components/createElementComponent.test.tsx
+++ src/components/createElementComponent.test.tsx
@@ -1,4 +1,5 @@
-import React, {StrictMode} from 'react';
+import * as React from 'react';
+import {StrictMode} from 'react';
 import {render, act, waitFor} from '@testing-library/react';
 
 import * as ElementsModule from './Elements';
